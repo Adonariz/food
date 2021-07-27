031cdb3a086c723cf0b61e72f645d7da64eca56a
A Zhila <aleksej.zhila@gmail.com>
2021-07-27 23:07:15 +0300

    добавляет таймер

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', ()=> {
+  // Tabs
+  
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -37,4 +39,58 @@ window.addEventListener('DOMContentLoaded', ()=> {
       });
     }
   });
+
+  // Timer
+
+  const deadline = '2021-08-15';
+
+  function getTimeRemaining(endtime) {
+    const time = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.floor(time / (1000 * 60 * 60 * 24)),
+          hours = Math.floor((time / (1000 * 60 * 60)) % 24),
+          minutes = Math.floor((time / 1000 / 60) % 60),
+          seconds = Math.floor((time / 1000) % 60);
+
+    return {
+      'total': time,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const time = getTimeRemaining(endtime);
+      
+      days.innerHTML = getZero(time.days);
+      hours.innerHTML = getZero(time.hours);
+      minutes.innerHTML = getZero(time.minutes);
+      seconds.innerHTML = getZero(time.seconds);
+
+      if (time.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
 });
